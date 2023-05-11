c0cad7d4dfecfa012de6d8852d64507e87bfd4bf
Viacheslav Borysov <s.borisovbar@gmail.com>
2023-05-12 00:56:36 +0300

    start working, add controllers and routes logic

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,21 +1,47 @@
 const express = require("express");
+const contactsService = require("../../models/contacts");
+const { HttpError } = require("../../helpers");
 
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "Start" });
+  try {
+    const result = await contactsService.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { contactId } = req.params;
+    const result = await contactsService.getContactById(contactId);
+    if (!result) {
+      throw HttpError(404, "This contacts not found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+  } catch (error) {}
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { contactId } = req.params;
+    const result = await contactsService.removeContact(contactId);
+    if (!result) {
+      throw HttpError(404, "Not Found");
+    }
+    res.status(200).json({ message: "contact deleted" });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.put("/:contactId", async (req, res, next) => {
